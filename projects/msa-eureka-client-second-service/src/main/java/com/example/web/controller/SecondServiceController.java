576342d9277282543430884c20b1880dbef746bf
이응수 <eungsu@me.com>
2022-08-02 07:32:17 +0900

    @Value 삭제

--- projects/msa-eureka-client-second-service/src/main/java/com/example/web/controller/SecondServiceController.java
+++ projects/msa-eureka-client-second-service/src/main/java/com/example/web/controller/SecondServiceController.java
@@ -1,6 +1,5 @@
 package com.example.web.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
