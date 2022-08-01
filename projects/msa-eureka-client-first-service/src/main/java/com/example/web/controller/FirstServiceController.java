02198ab1c41f65ffd347d9307beb540b72a44901
이응수 <eungsu@me.com>
2022-08-01 14:18:38 +0900

    Eureka Sever와 Spring Clound Gateway를 이용한 부하분산

--- projects/msa-eureka-client-first-service/src/main/java/com/example/web/controller/FirstServiceController.java
+++ projects/msa-eureka-client-first-service/src/main/java/com/example/web/controller/FirstServiceController.java
@@ -1,15 +1,24 @@
 package com.example.web.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
 
+	// 로드밸런스 동작 체크를 위해서 추가된 내용
+	// application.properties의 환경설정 정보를 제공하는 객체를 주입받는다.
+	private final Environment env;
+	
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome to the First service.";
+		//return "Welcome to the First service.";
+		return String.format("Hi, there. This is a message from first service on PORT %s", env.getProperty("local.server.port"));
 	}
 }
--- projects/msa-eureka-client-second-service/src/main/java/com/example/web/controller/SecondServiceController.java
+++ projects/msa-eureka-client-second-service/src/main/java/com/example/web/controller/SecondServiceController.java
@@ -1,15 +1,26 @@
 package com.example.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
 
+	// 로드밸런스 동작 체크를 위해서 추가된 내용
+	// application.properties의 환경설정 정보를 제공하는 객체를 주입받는다.
+	private final Environment env;
+		
+	
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome to the Second service.";
+		//return "Welcome to the Second service.";
+		return String.format("Hi, there. This is a message from second service on PORT %s", env.getProperty("local.server.port"));
 	}
 }
