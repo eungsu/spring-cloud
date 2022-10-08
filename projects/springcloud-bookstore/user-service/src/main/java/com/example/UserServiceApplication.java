603d5f922c14a5050d66c4a01d3f775026535d14
eungsu <eungsu@me.com>
2022-10-08 16:43:09 +0900

    북스토어 유저 서비스 생성 및 레지스트리 등록

--- projects/springcloud-bookstore/user-service/src/main/java/com/example/UserServiceApplication.java
+++ projects/springcloud-bookstore/user-service/src/main/java/com/example/UserServiceApplication.java
@@ -1,9 +1,12 @@
-package com.example.userservice;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// 유레카 클라이언트를 활성화시킨다.
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
