c5cf551a1867c1e020119a515c7bd8da797e9b15
eungsu <eungsu@me.com>
2022-10-11 00:21:25 +0900

    유저 마이크로서비스를 유레카 서버에 등록시키기

--- projects/springcloud-bookstore/user-service/src/main/java/com/example/UserServiceApplication.java
+++ projects/springcloud-bookstore/user-service/src/main/java/com/example/UserServiceApplication.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 // 유레카 클라이언트를 활성화시킨다.
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
